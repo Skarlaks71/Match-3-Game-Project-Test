bded2d89a51d753eb6ff71281454cba041fb4b93
Skarlaks71 <mapisaro07@gmail.com>
2020-06-18 18:45:28 -0300

    Create ramdon grid

--- Assets/Match 3 Starter/Scripts/Board and Grid/BoardManager.cs
+++ Assets/Match 3 Starter/Scripts/Board and Grid/BoardManager.cs
@@ -26,33 +26,58 @@ using System.Collections.Generic;
 
 public class BoardManager : MonoBehaviour {
 	public static BoardManager instance;
-	public List<Sprite> characters = new List<Sprite>();
-	public GameObject tile;
-	public int xSize, ySize;
+    [SerializeField]
+	private List<Sprite> _characters = new List<Sprite>();
+    [SerializeField]
+	private GameObject _tile;
+    [SerializeField]
+	private int _xSize, _ySize;
 
-	private GameObject[,] tiles;
+	private GameObject[,] _tiles;
 
 	public bool IsShifting { get; set; }
 
 	void Start () {
 		instance = GetComponent<BoardManager>();
 
-		Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
+		Vector2 offset = _tile.GetComponent<SpriteRenderer>().bounds.size;
         CreateBoard(offset.x, offset.y);
     }
 
 	private void CreateBoard (float xOffset, float yOffset) {
-		tiles = new GameObject[xSize, ySize];
+		_tiles = new GameObject[_xSize, _ySize];
 
         float startX = transform.position.x;
 		float startY = transform.position.y;
 
-		for (int x = 0; x < xSize; x++) {
-			for (int y = 0; y < ySize; y++) {
-				GameObject newTile = Instantiate(tile, new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), tile.transform.rotation);
-				tiles[x, y] = newTile;
-			}
+        Sprite[] previousLeft = new Sprite[_ySize];
+        Sprite previousBelow = null;
+
+        for (int x = 0; x < _xSize; x++) {
+			for (int y = 0; y < _ySize; y++) {
+                
+                GameObject newTile = Instantiate(_tile, new Vector3(startX + (xOffset * x), startY + (yOffset * y), 0), _tile.transform.rotation);
+				_tiles[x, y] = newTile;
+                newTile.transform.parent = transform; 
+
+                List<Sprite> possibleCharacters = new List<Sprite>();
+                possibleCharacters.AddRange(_characters);
+
+                possibleCharacters.Remove(previousLeft[y]);
+                possibleCharacters.Remove(previousBelow);
+
+                Sprite newSprite = possibleCharacters[Random.Range(0, possibleCharacters.Count)];
+
+                newTile.GetComponent<SpriteRenderer>().sprite = newSprite;
+                previousLeft[y] = newSprite;
+                previousBelow = newSprite;
+
+
+            }
         }
+
+        
+
     }
 
 }
